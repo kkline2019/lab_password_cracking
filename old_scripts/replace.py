9d0322e2d48d2de616622f2d9ec6d6c07599b49e
Adonay <adonaypichardo@gmail.com>
2021-09-07 15:55:28 -0400

    updated files, reformated update_word_list.py

---
+++ old_scripts/replace.py
@@ -0,0 +1,17 @@
+"""
+Author:  Adonay Pichardo
+Date:    07SEP2021
+Contact: apichardo2019@my.fit.edu
+"""
+
+from sys import argv # Used for command line arguments
+
+old_file = open(argv[1], "r")                # The original dictionary
+new_file = open(argv[0][:-3] + ".dict", "w") # The new altered dictionary
+
+for every_line in old_file.readlines():
+	every_line = every_line.replace(argv[2], argv[3])
+	new_file.write(every_password)
+
+old_file.close()
+new_file.close()
